c38c1e1a131abe22253ea7323ac5dbcf09bf64a3
Caleb York <caleb.york@fwcsstudents.org>
2024-08-30 12:25:18 -0400

    Add files via upload
    Choose two of the programs you’ve written, and add at least one comment to each. If you don’t have anything specific to write because your programs are too simple at this point, just add your name and the current date at the top of each program file. Then write one sentence describing what the program does.

---
+++ York_TIY_2_11.py
@@ -0,0 +1,8 @@
+print(5+3)
+print(11-3)
+print(4*2)
+print(int(16/2))
+# I used the int() to make 8.0 to 8
+favorite_number = 4
+print("My favorite number is", str(favorite_number) +'.')
+# Used str() so I would not get an error regarding adding numbers and strings
