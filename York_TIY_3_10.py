78debe646dbf2e0ee5eeaa387267ca82fec39234
Caleb York <caleb.york@fwcsstudents.org>
2024-09-03 13:23:22 -0400

    Add files via upload
    Create 3 separate empty lists then add 4 to 5 items in each list. Then use the functions in the book to manipulate the lists.

---
+++ York_TIY_3_10.py
@@ -0,0 +1,23 @@
+things = ["baseball","soccer ball", "tennis ball", "basketball", "football"]
+ideas = ["happy", "sad", "mean", "angry"]
+people = ["Henry", "Joe", "Paul", "Link"]
+print(things[3])
+poped = things.pop(2)
+print(things)
+things.append(poped)
+print(things)
+ideas.insert(2,"welp")
+print(ideas)
+del ideas[2]
+print(ideas)
+print(people)
+print(sorted(people))
+print(people)
+people.reverse()
+print(people)
+people.reverse()
+print(people)
+people.sort()
+print(people)
+people.sort(reverse=True)
+print(people)
