8e8f5799c9b63bb302fc0184565c12c694a700f5
Caleb York <caleb.york@fwcsstudents.org>
2024-09-11 13:21:32 -0400

    Add files via upload
    Write a series of conditional tests. Print a statement describing each test and your prediction for the results of each test. Your code should look something like this: car = 'subaru' print("Is car == 'subaru'? I predict True.") print(car == 'subaru') print("\nIs car == 'audi'? I predict False.") print(car == 'audi') • Look closely at your results, and make sure you understand why each line evaluates to True or False. • Create at least 10 tests. Have at least 5 tests evaluate to True and another 5 tests evaluate to False.

---
+++ York_TIY_6_1.py
@@ -0,0 +1,39 @@
+car = 'adui'
+print(f"Does car == bmw? I think False. " )
+print(car == "bmw")
+
+car = 'bmw'
+print("\nDoes car == bmw? I think True")
+print(car == 'bmw')
+
+car = 'honda'
+print("\nDoes car == bmw? I think False")
+print(car == 'bmw')
+
+car = 'toyota'
+print("\nDoes car == audi? I think False")
+print(car == 'audi')
+
+car = 'porche'
+print("\nDoes car == porche? I think True")
+print(car == 'porche')
+
+car = 'adui'
+print(f"Does car == audi? I think True. " )
+print(car == "audi")
+
+car = 'bmw'
+print("\nDoes car == toyota? I think False")
+print(car == 'toyota')
+
+car = 'honda'
+print("\nDoes car == honda? I think True")
+print(car == 'honda')
+
+car = 'toyota'
+print("\nDoes car == toyota? I think True")
+print(car == 'toyota')
+
+car = 'porche'
+print("\nDoes car == honda? I think False")
+print(car == 'honda')
