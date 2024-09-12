d98688380ca044b38cfafe91261a27b28c78c6f3
Caleb York <caleb.york@fwcsstudents.org>
2024-09-12 11:56:17 -0400

    Add files via upload

--- York_TIY_6_1.py
+++ York_TIY_6_1.py
@@ -1,39 +1,6 @@
-car = 'adui'
-print(f"Does car == bmw? I think False. " )
-print(car == "bmw")
+person = {"first_name" : "Denver", "last_name" : "Zararte-Garcia", "city" : "Fort Wayne", "foods" : "anything"}
 
-car = 'bmw'
-print("\nDoes car == bmw? I think True")
-print(car == 'bmw')
-
-car = 'honda'
-print("\nDoes car == bmw? I think False")
-print(car == 'bmw')
-
-car = 'toyota'
-print("\nDoes car == audi? I think False")
-print(car == 'audi')
-
-car = 'porche'
-print("\nDoes car == porche? I think True")
-print(car == 'porche')
-
-car = 'adui'
-print(f"Does car == audi? I think True. " )
-print(car == "audi")
-
-car = 'bmw'
-print("\nDoes car == toyota? I think False")
-print(car == 'toyota')
-
-car = 'honda'
-print("\nDoes car == honda? I think True")
-print(car == 'honda')
-
-car = 'toyota'
-print("\nDoes car == toyota? I think True")
-print(car == 'toyota')
-
-car = 'porche'
-print("\nDoes car == honda? I think False")
-print(car == 'honda')
+print(person["first_name"])
+print(person["last_name"])
+print(person["city"])
+print(person["foods"])
