8112948c2a13bd7d2d7963323cf1053227fad56a
Caleb York <caleb.york@fwcsstudents.org>
2024-09-18 12:27:41 -0400

    Delete York_TIY_3_5.py

--- York_TIY_3_5.py
+++
@@ -1,8 +0,0 @@
-invites = ["Notch", "Jeb_", "Babe Ruth"]
-for i in invites:
-    print(f"Dear {i}, you have been invited to dinner by Yorky.")
-print(f"{invites[1]} was not able to come.")
-invites.pop(1)
-invites.append("Joey Votto")
-for i in invites:
-    print(f"Dear {i}, you have been invited to dinner by Yorky.")
