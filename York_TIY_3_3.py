30bc0d9a86b235f156ca7ea4e6986c2cf049ee88
Caleb York <caleb.york@fwcsstudents.org>
2024-09-18 12:27:54 -0400

    Delete York_TIY_3_3.py

--- York_TIY_3_3.py
+++
@@ -1,8 +0,0 @@
-trans = ["skateboard", "longboard", "car", "bike", "electric bike", "motorcycle", "parachute"]
-print(f"I would love to do a kick flip on a {trans[0]}.")
-print(f"Flying down a hill on a {trans[1]} is fun.")
-print(f"I do not love my {trans[2]}.")
-print(f"{trans[3].title()}rides are a great way to exercise.")
-print(f"{trans[4].title()}s are not great for most.")
-print(f"{trans[5].title()}s stay up right when moving without someone on them.")
-print(f"Sky diving is fun when you have a {trans[6]}.")
